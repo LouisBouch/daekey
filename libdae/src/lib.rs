0e467621602bfa04d426ed5aa6d3ddfb724d7e90
serlij <louisbouchard@mail.com>
2026-09-04 23:25:09 -0400

    doc: add todos for next step

--- libdae/src/lib.rs
+++ libdae/src/lib.rs
@@ -14,13 +14,18 @@ pub use evdev::{RelativeAxisCode, AbsoluteAxisCode};
 pub use message::AppliedModifiers;
 pub type Pixel = i32;
 
-// TODO: Make privileged process a daemon. Have user launch individual processes that request
-// connection through a binary that requries sudo. Once connected, the new user launched app can
-// communicate with the daemon.
-// Each connection has a single connection to the daemon, and all requests go through it.
-// Make the daemon listen to compositor change and have it send the updated values to the list of connections.
-// Upon connecting, have the daemon send compositor values (like the screen layout an stuff) to the
-// processes connecting to it.
+// TODO: Order of things:
+//
+// 1. Remove creation of multiple sockets and keep only one socket to talk between the processes.
+// 2. Make the privileged process listen for compositor changes and push them to the
+//    core/unprivileged process. Also, have the daemon send the initial compositor values.
+// 3. Make the daemon create a single connection socket in the /run folder and only allow rooted
+//    processes to access it.
+// 4. Make the daemon independent from the core, that way they can be launched independently, and
+//    ensure the core, which is now just a normal connection to the daemon, can find the daemon's socket.
+// 5. Instead of shelling out to sudo for anything, always start with sudo privileges, and drop to
+//    minimum privileges as soon as possible (after setpriv for daemon and after connection to the
+//    socket for the "core" connections).
 
 // TODO: Create macro maker. Fetches initiale absolute cursor position on a key press and then
 // record everything that is done. But before doing it, clean up the code a bit.
