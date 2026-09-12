ba2853b6880934cb57696bd25b12612ecbb42cfc
serlij <louisbouchard@mail.com>
2026-09-12 00:23:29 -0400

    feat: add service binary creation

--- install.sh
+++ install.sh
@@ -3,5 +3,14 @@
 # Run script from location of this script.
 cd "$(dirname -- "${BASH_SOURCE[0]}")" || exit 1
 
-cargo build --release
-# TODO: cp the built binary to /usr/local/sbin/daekey
+if [[ "$1" == "--debug" ]]; then
+  target="debug"
+  cargo build -p bindae || exit 1
+else
+  target="release"
+  cargo build --release -p bindae || exit 1
+fi
+
+
+sudo cp target/"$target"/bindae /usr/local/sbin/
+# User must restart the service to use the updated binary.
