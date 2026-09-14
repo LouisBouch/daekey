f5dd2be05c312df9c9e9c1de81b65e1d5b23277a
serlij <louisbouchard@mail.com>
2026-09-14 17:38:32 -0400

    feat: add daekey dir creation in /run

--- bindae/src/main.rs
+++ bindae/src/main.rs
@@ -1,6 +1,11 @@
-mod socket;
+use std::time::Duration;
+
 mod manager;
+mod socket;
 
 fn main() {
     println!("Hello, world!");
+    loop {
+        std::thread::sleep(Duration::from_secs(20));
+    }
 }
