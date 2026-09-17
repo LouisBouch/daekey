8f849bebfa83b6791c4fc0e0104cd0ba78fcd16e
serlij <louisbouchard@mail.com>
2026-09-17 19:40:30 -0400

    refactor: remove unecessary generic type

--- bindae/src/socket/connection.rs
+++ bindae/src/socket/connection.rs
@@ -5,19 +5,17 @@ use std::{marker::PhantomData, os::unix::net::UnixStream};
 use serde::{Serialize, de::DeserializeOwned};
 
 /// A connection that receives messages from a connected process.
-pub struct ConnectionRx<R: DeserializeOwned> {
+pub struct ConnectionRx {
     /// Socket connecting a process to the daemon.
     socket: UnixStream,
-    /// The struct needs to hold the generic type somewhere.
-    _t: PhantomData<R>,
+    /// Buffer for the receiver.
+    buffer: Vec<u8>,
 }
 
 /// A connection that sends messages to a connected process.
-pub struct ConnectionTx<S: Serialize> {
+pub struct ConnectionTx {
     /// Socket connecting a process to the daemon.
     socket: UnixStream,
-    /// The struct needs to hold the generic type somewhere.
-    _t: PhantomData<S>,
 }
 // TODO:
 // - thread per Connection Rx. Then sends received messages to a logic thread that routes it
