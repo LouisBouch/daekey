ec2fdc1c468ef5af66c5c2c5e205866b16655553
serlij <louisbouchard@mail.com>
2026-09-22 17:07:55 -0400

    wip: add infrasctructure for input and uinput

--- bindae/src/device_interface/input.rs
+++ bindae/src/device_interface/input.rs
@@ -3,3 +3,12 @@
 /// Messages that can be sent to the input interface.
 pub enum InputMessage {
 }
+
+/// Holds necessary values to interface with input devices and talk back to the [`Router`].
+pub struct Input {
+}
+impl Input {
+    /// Launch the interface which will listen to input devices and message requests.
+    pub fn launch(&self) {
+    }
+}
--- bindae/src/device_interface/uinput.rs
+++ bindae/src/device_interface/uinput.rs
@@ -4,3 +4,12 @@
 /// Messages that can be sent to the uinput interface.
 pub enum UInputMessage {
 }
+
+/// Holds necessary values to interface with uinput and talk back to the [`Router`].
+pub struct UInput {
+}
+impl UInput {
+    /// Launch the interface which will listen to message requests.
+    pub fn launch(&self) {
+    }
+}
